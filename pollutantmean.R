49c91cc254c8e29f8d44c30956b776301ab61686
John Doody <40151857+JDoody@users.noreply.github.com>
2018-06-19 15:56:46 +0100

    Add files via upload

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,28 +1,21 @@
-
 # readfiletype("./specdata/1filetype")
 ## paste("0", filename, filetype, sep = "")
 
 pollutantmean <- function(directory = "specdata", pollutant = "sulfate", id = "001") {
-  pollutantcount <- 0
+  
+  files <- list.files(path = directory, id, pattern = paste(".csv$", sep=""), full.names = TRUE)
+  
+  # Since you build this up you first need a starting value
   pollutantsum <- 0
-  
-  filetype <- ".csv"
-  pathswitch <- "."
-  
-
-  file_len <- nchar(id)
-  if (file_len == 1) {
-    new_id <- paste("00", id, filetype, sep = "")}
-  else if (file_len == 2) {
-    new_id <- paste("0", id, filetype, sep = "")}
-  else
-    new_id <- paste(id, filetype, sep = "")
-    new_path <-  file.path(pathswitch,directory, new_id)
-     new_data <- read.csv(new_path)
+  pollutantcount <- 0
+  # Use the files argument as the 'seq' argument
+  for (file in files[id]) {
+    new_data <- read.csv(paste0(file, sep = ""))
     pollutantsum <- pollutantsum + sum(new_data[,pollutant], na.rm=TRUE)
-    pollutantcount <- pollutantcount + length(na.exclude(new_data[,pollutant])) 
-    meanofpollutant <- (pollutantsum / pollutantcount) 
-    print(meanofpollutant)
- }
-
-
+    pollutantcount <- pollutantcount + length(na.exclude(new_data[,pollutant])) }
+    meanofpollutant <- (pollutantsum / pollutantcount)
+    
+    print(c("Count is ", pollutantcount))
+    print(c("Mean is ", meanofpollutant))
+  
+}
